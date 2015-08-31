390edac06cae8a45f3351de93b7127f139d9af3b
gitmehedi <git.mehedi@gmail,.com>
2015-08-31 20:33:48 +0600

    add new controller

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -1,5 +1,18 @@
 var myApp = angular.module('myApp', []);
-myApp.controller('myCtrl', function ($scope) {
+
+
+myApp.controller('myCtrl', function ($scope, $http) {
     $scope.message = "Hello Plunker!";
+    $scope.persons;
+
+    $http.get('https://api.github.com/users/hasin')
+            .then(githubUser);
+
+    var githubUser = function (response) {
+        console.log(response);
+        $scope.persons = response;
+    };
+
 });
 
+//app.config('')
