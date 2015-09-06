2c43f3b5a7f9df9aa7841966b34d4ddc6e28f231
nopaws <md.mehedi.info@gmail.com>
2015-09-06 23:37:17 +0600

    [MOD] Modify on formsController.js

--- app/controller/formsController.js
+++ app/controller/formsController.js
@@ -2,4 +2,18 @@ var app = angular.module('myApp', []);
 
 app.controller('UserController', function ($scope) {
     $scope.user = {};
+    $scope.countries = [
+        {
+            'id': 'US',
+            'desc': 'United States'
+        },
+        {
+            'id': 'GB',
+            'desc': 'United Kingdom'
+        },
+        {
+            'id': 'AU',
+            'desc': 'Australia'
+        }
+    ];
 });
