47ec5f4bd5311b92cd45967d830cd0e2c40160b7
hasan.mehedi <hasanmehedi@hasan.mehdi>
2016-09-04 12:32:56 +0600

    [DRAFT] Add some functionality

--- app/modules/posts/postModule.js
+++ app/modules/posts/postModule.js
@@ -1,4 +1,5 @@
-var postModule = angular.module('postModule',[]);
+var postModule = angular.module('postModule',['ngRoute']);
+
 
 postModule.controller('blogCtrl',function($scope){
     $scope.blogs=[
@@ -7,7 +8,18 @@ postModule.controller('blogCtrl',function($scope){
         {title:'Title 3',author_name:'Mehedi Hasan',email:'md.mehedi.info@gmail.com',publish_date:'12/12/2016'},
         {title:'Title 4',author_name:'Mehedi Hasan',email:'md.mehedi.info@gmail.com',publish_date:'12/12/2016'},
         ];
-    $scope.addBlog= function ($scope) {
-        console.log('Man, you are doing great :D');
-    }
+    // $scope.addBlog= function ($scope) {
+    //     console.log('Man, you are doing great :D');
+    // }
 });
+
+
+postModule.config(['$routeProvider','$locationProvider',function ($routeProvider,$locationProvider) {
+    $routeProvider.when('/blog',{
+        controller:'blogCtrl',
+        templateUrl: 'views/blog.html'
+    }).when('/blog-admin',{
+        controller: 'blogCtrl',
+        templateUrl: 'views/blog.html'
+    });
+}]);
