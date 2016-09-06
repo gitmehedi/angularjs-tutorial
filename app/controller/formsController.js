c0240fae87ab17dd983402eff54e3ac98c2714a5
hasan.mehedi <hasanmehedi@hasan.mehdi>
2016-09-06 17:49:40 +0600

    [MOD] Change on angular Forms

--- app/controller/formsController.js
+++ app/controller/formsController.js
@@ -1,19 +1,37 @@
 var app = angular.module('myApp', []);
 
 app.controller('UserController', function ($scope) {
-    $scope.user = {};
+    $scope.users = [];
     $scope.countries = [
         {
-            'id': 'US',
-            'desc': 'United States'
+            id: 'US',
+            desc: 'United States'
         },
         {
-            'id': 'GB',
-            'desc': 'United Kingdom'
+            id: 'GB',
+            desc: 'United Kingdom'
         },
         {
-            'id': 'AU',
-            'desc': 'Australia'
+            id: 'AU',
+            desc: 'Australia'
         }
     ];
+    $scope.saveUser= function() {
+        if($scope.myform.$valid){
+            $scope.users.push({
+                id:'23',
+                firstName:$scope.user.firstName,
+                lastName:$scope.user.lastName,
+                phoneNumber:$scope.user.phoneNumber
+            });
+            $scope.resetForm();
+        }else{
+            console.log('Unable to save. Validation error!');
+        }
+    };
+    $scope.resetForm=function(){
+        $scope.user ={};
+        $scope.myform.$setPristine();
+        $scope.myform.$setUntouched();
+    };
 });
