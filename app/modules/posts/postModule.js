d64a9b154589174a546809641d607def504c81a8
Mehedi Hasan <git.mehedi@gmail.com>
2016-09-07 15:56:11 +0600

    [MOD] Change on forms controller in angularJS

--- app/modules/posts/postModule.js
+++ app/modules/posts/postModule.js
@@ -15,45 +15,55 @@ postModule.controller('blogCtrl', function ($scope) {
         {id: 'usa', desc: 'United States America'},
         {id: 'uk', desc: 'United Kingdom'},
     ];
-    // $scope.addBlog= function ($scope) {
-    //     console.log('Man, you are doing great :D');
-    // }
-    $scope.saveUser = function () {
-        $scope.blogs.push({
-            title: $scope.title,
-            author_name: $scope.author_name,
-            email: $scope.email,
-            publish_Date: new Date(),
-        });
-    }
+
+    $scope.saveUser= function() {
+        console.log('Done Man >_<');
+        if($scope.myform.$valid){
+            console.log($scope.blog.title);
+            console.log($scope.blog.author_name);
+            console.log($scope.blog.email);
+            $scope.blogs.push({
+                title: $scope.blog.title,
+                author_name: $scope.blog.author_name,
+                email: $scope.blog.email,
+                publish_date: new Date()
+            });
+            console.log($scope.blogs);
+            $scope.resetForm();
+        }else{
+            console.log('Unable to save. Validation error!');
+        }
+        console.log($scope);
+    };
+    $scope.resetForm=function(){
+        $scope.blog ={};
+        $scope.myform.$setPristine();
+        $scope.myform.$setUntouched();
+    };
 });
 
 
+
+// configuration options and code goes here
 postModule.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
     $routeProvider
-    // route for the blog page
         .when('/blog', {
             templateUrl: 'app/modules/posts/views/blog.html',
             controller: 'blogCtrl'
         })
-
-        //route for the blog-admin page
         .when('/blog-admin', {
             templateUrl: 'app/modules/posts/views/blog-admin.html',
             controller: 'blogCtrl'
         })
-
-        // route for new blog page
         .when('/new', {
             templateUrl: 'app/modules/posts/views/forms.html',
             controller: 'blogCtrl'
         })
-
-        // route for edit a blog page
         .when('/edit', {
             templateUrl: 'app/modules/posts/views/blog-admin.html',
             controller: 'blogCtrl'
         });
+    // $locationProvider.html5Mode(true);
 
 }]);
